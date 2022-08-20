998d31e8c3e8f3e93d2f82981a3a5b8e7a0629c5
Andrey Mukhin <Andrey_Mukhin@russianpost.ru>
2022-08-20 12:59:07 +0300

    Initial Commit

---
+++ PDFReader/PDFReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFReaderApp.swift
+//  PDFReader
+//
+//  Created by Andrey Mukhin on 20.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct PDFReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
